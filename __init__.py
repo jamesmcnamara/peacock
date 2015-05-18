7c7a6e2d640d57c39ba107c24f88be37505995c1
jamesmcnamara <jamesscottmcnamara@gmail.com>
2015-05-18 18:00:41 -0400

    readded init file

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .peacock.peacock import Peacock
+from .peacock.mode import Mode, ModeError
+from .format import format 
+from .interact import interact, keyboard
+# Initialize the format closure
+
